eaaa696d7e4eee78de60d5fcbe84371b89373d86
royvandewater <royvandewater@users.noreply.github.com>
2020-05-30 00:12:52 +0000

    deploy: 98ac5897e915483c7a76b4555b01d0f6fa48e0db

---
+++ precache-manifest.d0575d2e86a13a37c081f729c53cf87e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e2711dd4f3aa79e56c8507c355ed5c",
+    "url": "/expert-potato/index.html"
+  },
+  {
+    "revision": "6ec349402b5bf3b0ad59",
+    "url": "/expert-potato/static/css/main.871f269a.chunk.css"
+  },
+  {
+    "revision": "f93a487fa4be5080a41e",
+    "url": "/expert-potato/static/js/2.31bd7176.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/expert-potato/static/js/2.31bd7176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec349402b5bf3b0ad59",
+    "url": "/expert-potato/static/js/main.c66f576e.chunk.js"
+  },
+  {
+    "revision": "0a91ff8f6fc58c24890d",
+    "url": "/expert-potato/static/js/runtime-main.82a00ceb.js"
+  }
+]);
